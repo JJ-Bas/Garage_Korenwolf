c48d83aebbcbbaefe7bb77ef3a343e8cf50f9a22
JJ-Bas <jef.j.bas@gmail.com>
2025-06-23 13:18:29 +0200

    eerste commit

---
+++ src/main/java/com/novi/garage_korenwolf/GarageKorenwolfApplication.java
@@ -0,0 +1,13 @@
+package com.novi.garage_korenwolf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageKorenwolfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageKorenwolfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novi/garage_korenwolf/GarageKorenwolfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novi.garage_korenwolf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageKorenwolfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
