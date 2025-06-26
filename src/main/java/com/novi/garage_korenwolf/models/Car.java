c45b680e2a14875a1de4bd62b23d279221143cc5
JJ-Bas <jef.j.bas@gmail.com>
2025-06-26 20:32:56 +0200

    colum namen toegevoegd aan Car en Person models

--- src/main/java/com/novi/garage_korenwolf/models/Car.java
+++ src/main/java/com/novi/garage_korenwolf/models/Car.java
@@ -14,13 +14,21 @@ public class Car {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "owner")
     private String ownerName;
+    @Column(name = "numberplate")
     private String numberplate;
+    @Column(name = "registration_date")
     private LocalDate registrationDate;
+    @Column(name = "build_year")
     private int buildYear;
+    @Column(name = "color", length = 64)
     private String color;
+    @Column(name = "fuel_type")
     private String fuelType;
+    @Column(name = "brand_name")
     private String Make;
+    @Column(name = "model_name")
     private String Model;
 
     //getters
--- src/main/java/com/novi/garage_korenwolf/models/Person.java
+++ src/main/java/com/novi/garage_korenwolf/models/Person.java
@@ -1,9 +1,6 @@
 package com.novi.garage_korenwolf.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
@@ -15,13 +12,21 @@ public class Person {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "first_name", length = 128)
     private String firstname;
+    @Column(name = "last_name", length = 128)
     private String lastname;
+    @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
+    @Column(name = "street_name", length = 128)
     private String street;
+    @Column(name = "house_number")
     private int houseNumber;
+    @Column(name= "postal_code")
     private String postalCode;
+    @Column(name = "telephone_number")
     private int telephoneNumber;
+    @Column(name = "email")
     private String email;
 
     //TODO foreign key: car & role & autopapieren uploaden
