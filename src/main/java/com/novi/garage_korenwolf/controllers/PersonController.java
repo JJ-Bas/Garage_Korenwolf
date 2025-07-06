d91ff8149567647eb7e4822fd491c3f158891c27
JJ-Bas <jef.j.bas@gmail.com>
2025-07-06 20:47:34 +0200

    dependency spring-boot-starter-validation toegevoeg, PersonDto validation constraints toegevoegd,  person controller postmapping valideer controleert validatie en geeft uitgebreide foutmelding terug

--- src/main/java/com/novi/garage_korenwolf/controllers/PersonController.java
+++ src/main/java/com/novi/garage_korenwolf/controllers/PersonController.java
@@ -4,12 +4,17 @@ import com.novi.garage_korenwolf.dto.PersonDto;
 import com.novi.garage_korenwolf.models.Person;
 import com.novi.garage_korenwolf.repositories.PersonRepository;
 import com.novi.garage_korenwolf.services.PersonService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/persons")
@@ -29,20 +34,35 @@ public class PersonController {
     }
 
     @PostMapping
-    public ResponseEntity<PersonDto> createPerson(@RequestBody PersonDto personDto) {
-        service.createPerson(personDto);
+    public ResponseEntity<Object> createPerson(@Valid @RequestBody PersonDto personDto, BindingResult br) {
+        //@Valid/BindingResult br zorgen er voor dat ieder veld in personDto word gecontroleerd op een geldige invoer
+        //Als 1 of meerdere br velden fouten bevatten wordt een loop afgedraaid die alle velden naloopt op foutmeldingen
+        // en deze worden omgezet naar een string die wordt meegegeven aan de ResponsEntity
+        //ResponseEntity is een <Object> omdat deze zowel een String of een PersonDto kan teruggeven afhankelijk van de invoer
+        if (br.hasFieldErrors()) {
+            StringBuilder sb = new StringBuilder();
+            for (FieldError fe : br.getFieldErrors()) {
+                sb.append(fe.getField());
+                sb.append(" : ");
+                sb.append(fe.getDefaultMessage());
+                sb.append("\n");
+            }
+            return ResponseEntity.badRequest().body(sb.toString());
+        } else {
+            service.createPerson(personDto);
 
-        //pakt uri van de huidige request en plakt daar de Id van de nieuw aangemaakt person aan vast.
-        //vervolgens word in de return de nieuwe person opgevraagd en meegegeven
+            //pakt uri van de huidige request en plakt daar de Id van de nieuw aangemaakt person aan vast.
+            //vervolgens word in de return de nieuwe person opgevraagd en meegegeven
 
-        URI uri = URI.create(ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/" + personDto.id).toUriString());
-        return ResponseEntity.created(uri).body(personDto);
+            URI uri = URI.create(ServletUriComponentsBuilder
+                    .fromCurrentRequest()
+                    .path("/" + personDto.id).toUriString());
+            return ResponseEntity.created(uri).body(personDto);
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PersonDto> updatePerson(@PathVariable Long id, @RequestBody PersonDto updatedPersonDto) {
+    public ResponseEntity<PersonDto> updatePerson(@Valid @PathVariable Long id, @RequestBody PersonDto updatedPersonDto, BindingResult br) {
 
         PersonDto updatedDto = service.updatePerson(id, updatedPersonDto);
         if (updatedDto != null) {
--- src/main/java/com/novi/garage_korenwolf/dto/PersonDto.java
+++ src/main/java/com/novi/garage_korenwolf/dto/PersonDto.java
@@ -1,6 +1,10 @@
 package com.novi.garage_korenwolf.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
@@ -8,12 +12,17 @@ public class PersonDto {
 
     public Long id;
 
+    @NotBlank
     public String firstname;
+    @NotBlank
     public String lastname;
+    @Past
     public LocalDate dateOfBirth;
     public String street;
+    @Size(min=1, max=10)
     public String houseNumber;
     public String postalCode;
     public int telephoneNumber;
+    @Email
     public String email;
 }
