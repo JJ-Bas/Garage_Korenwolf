ed496a3d0849dff83f317491d4426add9c4552a6
JJ-Bas <jef.j.bas@gmail.com>
2025-07-21 16:15:40 +0200

    SecurityConfig: UserDetailService methode toegevoegd

--- src/main/java/com/novi/garage_korenwolf/config/SecurityConfig.java
+++ src/main/java/com/novi/garage_korenwolf/config/SecurityConfig.java
@@ -2,23 +2,45 @@ package com.novi.garage_korenwolf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())            // Disable CSRF for tools like Postman
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()            // 🔓 Allow everything
-                )
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        InMemoryUserDetailsManager memoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails ud1 = User.builder()
+                .username("user")
+                .password(encoder.encode("password"))
+                .roles("USER")
                 .build();
+
+        UserDetails ud2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("adminpass"))
+                .roles("ADMIN")
+                .build();
+
+        memoryUserDetailsManager.createUser(ud1);
+        memoryUserDetailsManager.createUser(ud2);
+
+        return memoryUserDetailsManager;
     }
 }
 
-// TODO bean: password encoder
-// TODO bean: authenticatie manager
+
 // TODO bean: authorisation manager 
