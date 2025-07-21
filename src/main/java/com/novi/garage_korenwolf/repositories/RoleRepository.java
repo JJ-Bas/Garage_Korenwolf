d25cfe43c5e0dda4665c017132b889249e230b83
JJ-Bas <jef.j.bas@gmail.com>
2025-07-21 17:41:58 +0200

    interface RoleRepository toegevoeg

---
+++ src/main/java/com/novi/garage_korenwolf/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.novi.garage_korenwolf.repositories;
+
+import com.novi.garage_korenwolf.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
