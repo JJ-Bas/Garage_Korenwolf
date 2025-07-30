66e38e471534909d9a27998d5a68f51740b14807
JJ-Bas <jef.j.bas@gmail.com>
2025-07-30 16:50:30 +0200

    WorkPerformedService gevuld met methodes

--- src/main/java/com/novi/garage_korenwolf/services/WorkPerformedService.java
+++ src/main/java/com/novi/garage_korenwolf/services/WorkPerformedService.java
@@ -1,7 +1,44 @@
 package com.novi.garage_korenwolf.services;
 
+import com.novi.garage_korenwolf.models.WorkPerformed;
+import com.novi.garage_korenwolf.repositories.WorkPerformedRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WorkPerformedService {
+
+    private final WorkPerformedRepository repository;
+
+    public WorkPerformedService(WorkPerformedRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<WorkPerformed> getAllWorkPerformed() {
+        return repository.findAll();
+    }
+
+    public Optional<WorkPerformed> getWorkPerformedById(Long id) {
+        return repository.findById(id);
+    }
+
+    public WorkPerformed saveWorkPerformed(WorkPerformed workPerformed) {
+        return repository.save(workPerformed);
+    }
+
+    public WorkPerformed updateWorkPerformed(WorkPerformed workPerformed) {
+        return repository.save(workPerformed);
+    }
+
+    public void deleteWorkPerformedById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public boolean existsById(Long id) {
+        return repository.existsById(id);
+    }
 }
+
+
